e9f720441db6f1dba6d35613e2a10f92749dd7e5
joyseeyendluri <joyseeyendluri11888@gmail.com>
2024-06-07 16:26:39 +0530

    joysee

--- otp.java
+++ otp.java
@@ -1,7 +1,20 @@
-import java.util,*;
+import java.util
+import java.util.Scanner;,*;
 
 class otp{
     public static void main(String[]args)}
     Scanner sc=new
     Scanner(System.in);
-System.out.println(" enter only 4-degit")
+System.out.println(" enter only 4-degit numbers:");
+int a =sc.nextInt();
+int b =sc.nextInt();
+int c =sc.nextInt();
+int d =sc.nextInt();
+int otp= 0 ;
+//ope-1
+int th1=(a/1000)%10;
+int h1=(a/100)%10;
+int t1=(a/10)%10;
+int u1=(a/1)%10;
+ 
+
